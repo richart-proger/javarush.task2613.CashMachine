a3a2836780af9f426a17e76d2b2e2edd4dd6b1af
Artur Galimov <satorica@mail.ru>
2021-06-23 14:44:25 +0300

    feat: Add able to deposit money by currency code
    Read the currency code from the console.
    Count the denomination and number of banknotes.
    Then add them to the manipulator.
    Add all the described logic to the main method

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -7,17 +7,55 @@ import java.io.InputStreamReader;
 public class ConsoleHelper {
     private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void writeMessage(String message){
+    public static void writeMessage(String message) {
         System.out.println(message);
     }
 
-    public static String readString(){
+    public static String readString() {
         try {
             String string = bis.readLine();
             return string;
         } catch (IOException e) {
-            e.printStackTrace();
+            // IGNORE
         }
         return null;
+    }
+
+    public static String askCurrencyCode() {
+        String currencyCode;
+        while (true) {
+            ConsoleHelper.writeMessage("Please chose a currency code, for example USD");
+            currencyCode = ConsoleHelper.readString().trim();
+
+            if (currencyCode.length() == 3) {
+                break;
+            }
+            ConsoleHelper.writeMessage("Please specify valid data");
+        }
+        return currencyCode.toUpperCase();
+    }
+
+    public static String[] getValidTwoDigits(String currencyCode) {
+        String[] nums;
+
+        while (true) {
+            ConsoleHelper.writeMessage(String.format("Please specify integer denomination and integer count. For example '10 3' means 30 %s", currencyCode));
+            String string = ConsoleHelper.readString();
+
+            nums = string.split(" ");
+
+            if (nums.length == 2) {
+                    try {
+                        if (Integer.parseInt(nums[0]) <= 0 || Integer.parseInt(nums[1]) <= 0)
+                            throw new NumberFormatException();
+
+                    } catch (NumberFormatException e) {
+                        ConsoleHelper.writeMessage("Please specify valid data");
+                        continue;
+                    }
+                return nums;
+            }
+            ConsoleHelper.writeMessage("Please specify valid data");
+        }
     }
 }
--- CurrencyManipulator.java
+++ CurrencyManipulator.java
@@ -15,4 +15,13 @@ public class CurrencyManipulator {
     public String getCurrencyCode() {
         return currencyCode;
     }
+
+    public void addAmount(int denomination, int count){
+        if (denominations.containsKey(denomination)){
+            denominations.put(denomination, denominations.get(denomination) + 1);
+        }
+        else {
+            denominations.put(denomination, count);
+        }
+    }
 }
