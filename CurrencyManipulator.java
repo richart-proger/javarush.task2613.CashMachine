3caa35eb155d9304fbd2fdf1ad641ecf55680ff1
Artur Galimov <satorica@mail.ru>
2021-06-24 12:53:11 +0300

    feat: Implement the logic in WithdrawCommand
    Request all necessary data from user (currency code, amount), check
    their correctness.
    Check whether there are enough funds in the account.
    Withdraw money from the account using the new withdrawAmount() method.
    Output to the user.
    Sorting - from a larger denomination to a smaller one.
    Display a message about a successful transaction.
    Catch NotEnoughMoneyException.
    In case of a shortage of money on the account, notify the user about it.
    
    The logic of the main withdrawAmount method:
    withdrawAmount method returns the minimum number of banknotes that the
    requested amount is typed with.
    If there are several options, then use the one with the maximum number
    of banknotes of the highest denomination.
    If we have found such an option for issuing money, then subtract all
    these banknotes from the card in the manipulator.
    withdrawAmount method throw NotEnoughMoneyException if it is impossible
    to issue the requested amount in banknotes.

--- CurrencyManipulator.java
+++ CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task26.task2613;
 
-import java.util.Map;
-import java.util.TreeMap;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.*;
 
 public class CurrencyManipulator {
     private Map<Integer, Integer> denominations;
@@ -16,16 +17,15 @@ public class CurrencyManipulator {
         return currencyCode;
     }
 
-    public void addAmount(int denomination, int count){
-        if (denominations.containsKey(denomination)){
+    public void addAmount(int denomination, int count) {
+        if (denominations.containsKey(denomination)) {
             denominations.put(denomination, denominations.get(denomination) + count);
-        }
-        else {
+        } else {
             denominations.put(denomination, count);
         }
     }
 
-    public int getTotalAmount(){
+    public int getTotalAmount() {
         int amount = 0;
         for (Integer denomination : denominations.keySet()) {
             int count = denominations.get(denomination);
@@ -34,7 +34,55 @@ public class CurrencyManipulator {
         return amount;
     }
 
-    public boolean hasMoney(){
+    public boolean hasMoney() {
         return !denominations.isEmpty();
+    }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+        return expectedAmount <= getTotalAmount();
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        int expectedSum = expectedAmount;
+        HashMap<Integer, Integer> copyDenominations = new HashMap<>(denominations);
+
+        ArrayList<Integer> keysDenom = new ArrayList<>(this.denominations.keySet());
+
+        Collections.sort(keysDenom);
+        Collections.reverse(keysDenom);
+
+        Map<Integer, Integer> resultMap = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+
+        for (Integer denomination : keysDenom) {
+            final int keyDenom = denomination;
+            int valueCount = copyDenominations.get(denomination);
+
+            while (true) {
+                if (expectedSum < keyDenom || valueCount == 0) {
+                    copyDenominations.put(keyDenom, valueCount);
+                    break;
+                }
+                expectedSum -= keyDenom;
+                valueCount--;
+
+                if (resultMap.containsKey(keyDenom))
+                    resultMap.put(keyDenom, resultMap.get(keyDenom) + 1);
+                else
+                    resultMap.put(keyDenom, 1);
+            }
+        }
+
+        if (expectedSum > 0) {
+            throw new NotEnoughMoneyException();
+        } else  {
+            this.denominations.clear();
+            this.denominations.putAll(copyDenominations);
+        }
+        return resultMap;
     }
 }
--- command/WithdrawCommand.java
+++ command/WithdrawCommand.java
@@ -1,7 +1,49 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Map;
+
 class WithdrawCommand implements Command {
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException {
+        ConsoleHelper.writeMessage("Withdrawing...");
+
+        String currencyCode = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+
+        while (true) {
+            try {
+                ConsoleHelper.writeMessage("Please specify integer amount for withdrawing.");
+                String someAmount = ConsoleHelper.readString();
+
+                if (someAmount == null) {
+                    ConsoleHelper.writeMessage("Please specify valid positive integer amount for withdrawing.");
+                } else {
+                    try {
+                        int amount = Integer.parseInt(someAmount);
+                        boolean isAmountAvailable = manipulator.isAmountAvailable(amount);
+                        if (isAmountAvailable) {
+                            Map<Integer, Integer> denominations = manipulator.withdrawAmount(amount);
+                            for (Integer denomination : denominations.keySet()) {
+                                ConsoleHelper.writeMessage("\t" + denomination + " - " + denominations.get(denomination));
+                            }
+                            ConsoleHelper.writeMessage(String.format("%d %s was withdrawn successfully", amount, currencyCode));
+                            break;
+                        } else {
+                            ConsoleHelper.writeMessage("Not enough money on your account, please try again");
+                        }
+                    } catch (NumberFormatException e) {
+                        ConsoleHelper.writeMessage("Please specify integer amount for withdrawing.");
+                    }
+                }
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage("Exact amount is not available");
+            }
+        }
     }
 }
