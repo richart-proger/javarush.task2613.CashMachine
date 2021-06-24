1cd488090ad884f020122d5b57a063f5a2ed6e20
Artur Galimov <satorica@mail.ru>
2021-06-24 16:10:18 +0300

    refactor: Add resource with card's data of users
    Add a new resources package, which stores resource files (password=pin).
    Сhange the logic of LoginCommand working with resource files.

--- command/LoginCommand.java
+++ command/LoginCommand.java
@@ -1,11 +1,13 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 
+import java.util.ResourceBundle;
+
 public class LoginCommand implements Command{
-    String validCardNumber = "123456789012";
-    String validCardPin = "1234";
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.verifiedCards");
 
     @Override
     public void execute() throws InterruptOperationException {
@@ -23,7 +25,7 @@ public class LoginCommand implements Command{
                         " - 12 digits, pin code - 4 digits.");
             } else {
                 try {
-                    if (number.equals(validCardNumber) && pin.equals(validCardPin)){
+                    if (validCreditCards.containsKey(number) && pin.equals(validCreditCards.getString(number))){
                         ConsoleHelper.writeMessage(String.format("Credit card [%s] is verified successfully!", number));
                         break;
                     } else {
