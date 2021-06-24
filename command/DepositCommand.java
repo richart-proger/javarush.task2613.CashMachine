e53015c86f7e97b8887da64ffbd5f2a1ee667c56
Artur Galimov <satorica@mail.ru>
2021-06-24 16:46:30 +0300

    refactor: DepositCommand, ExitCommand, InfoCommand
    For each of these classes, add *.properties file with the appropriate
    logic, make them refactoring.

--- command/DepositCommand.java
+++ command/DepositCommand.java
@@ -1,14 +1,19 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.CurrencyManipulator;
 import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 
+import java.util.ResourceBundle;
+
 class DepositCommand implements Command {
+    private ResourceBundle res = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.deposit");
+
     @Override
     public void execute() throws InterruptOperationException {
-        ConsoleHelper.writeMessage("Depositing...");
+        ConsoleHelper.writeMessage(res.getString("before"));
         String currencyCode = ConsoleHelper.askCurrencyCode();
         CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
 
@@ -20,10 +25,10 @@ class DepositCommand implements Command {
                 int count = Integer.parseInt(digits[1]);
 
                 manipulator.addAmount(denomination, count);
-                ConsoleHelper.writeMessage(String.format("%d %s was deposited successfully", (denomination * count), currencyCode));
+                ConsoleHelper.writeMessage(String.format(res.getString("success.format"), (denomination * count), currencyCode));
                 break;
             } catch (NumberFormatException e) {
-                ConsoleHelper.writeMessage("Please specify valid data.");
+                ConsoleHelper.writeMessage(res.getString("invalid.data"));
             }
         }
     }
